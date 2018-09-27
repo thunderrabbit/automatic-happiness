7f4e851f293a003be59f5d34f8dde6419fe9d73d
Rob Nugen <rob@robnugen.com>
2018-09-27 13:04:10 +0900

    Can upload multiple construction pics at once

--- upconstruction.sh
+++ upconstruction.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This is designed to make it easier to copy images from finder to b.robnugen.com
 
-scp $1 b.rn:mt3/construction/2018
+UPTODIR="mt3/construction/2018"
+
+# $@ gives all the command line arguments
+scp $@ b.rn:$UPTODIR
 
 
