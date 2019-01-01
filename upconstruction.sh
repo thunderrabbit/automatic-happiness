b7f5864a9834ca44628ef71f64169293d9f945ec
Rob Nugen <rob@robnugen.com>
2019-01-01 13:23:43 +0900

    Update upload script dates to 2019
    TODO: make upload scripts use system year automatically

--- upconstruction.sh
+++ upconstruction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is designed to make it easier to copy images from finder to b.robnugen.com
 
-UPTODIR="mt3/construction/2018"
+UPTODIR="mt3/construction/2019"
 
 # $@ gives all the command line arguments
 scp $@ b.rn:$UPTODIR
--- upparts.sh
+++ upparts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is designed to make it easier to copy images from finder to b.robnugen.com
 
-scp $@ b.rn:mt3/track/parts/
+scp $@ b.rn:mt3/track/parts/2019
